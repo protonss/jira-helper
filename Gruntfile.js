547cc4b6265c193f2b3fede334e625201bc50fa3
Roberto Salinas <jose.salinas2@wal-mart.com>
2015-04-30 10:11:19 -0300

    install grunt and add gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,28 @@
+module.exports = function(grunt) {
+  "use strict";
+
+  grunt.initConfig({
+    package: grunt.file.readJSON("package.json"),
+    uglify: {
+      dist: {
+        files: {
+          "dist/scripts/main.min.js": [
+            "bower_components/jquery/jquery.min.js",
+            "js/jquery.balloon.min.js",
+            "bower_components/highcharts/highcharts.js",
+            "js/injectScript.js"
+          ]
+        }
+      }
+    },
+    watch: {
+      files: [],
+      tasks: []
+    }
+  });
+
+  grunt.loadNpmTasks("grunt-contrib-uglify");
+  grunt.loadNpmTasks("grunt-contrib-watch");
+
+  grunt.registerTask("default", ["uglify"]);
+};
